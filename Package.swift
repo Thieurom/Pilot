f44203ed60f46dcd4bb7e4cd5c4ba2ab6b577861
Doan Thieu <thieurom@gmail.com>
2022-11-14 10:50:51 +0700

    Add GitHub action to run tests, set Swift version to 5.5

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,13 +6,16 @@ import PackageDescription
 let package = Package(
     name: "Pilot",
     platforms: [
-        .iOS(.v13), .macOS(.v10_15), .macCatalyst(.v13), .tvOS(.v13), .watchOS(.v6)
+        .iOS(.v13),
+        .macOS(.v10_15),
+        .tvOS(.v13),
+        .watchOS(.v6)
     ],
     products: [
         .library(
             name: "Pilot",
             targets: ["Pilot"]
-        ),
+        )
     ],
     dependencies: [],
     targets: [
